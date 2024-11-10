d7801ea463a6f9fb72861dd9b567a887398cc35f
Sri Sai Deep Duduka <73338199+Saideep3376@users.noreply.github.com>
2024-11-10 20:39:40 +0530

    Update LoanAccount.java

--- LoanAccount.java
+++ LoanAccount.java
@@ -1,78 +1,163 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
-import java.io.PrintStream;
 import java.time.LocalDate;
 
-public class LoanApplication {
-   private static int applicationCounter = 1;
-   private String applicationId = this.generateApplicationId();
-   private Customer customer;
-   private double loanAmount;
-   private String loanType;
-   private String loanStatus;
-   private boolean verified;
-   private Employee verifiedBy;
-   private LocalDate applicationDate;
-   private LocalDate approvalDate;
+public class LoanAccount {
 
-   public LoanApplication(Customer var1, double var2, String var4, LocalDate var5) {
-      this.customer = var1;
-      this.loanAmount = var2;
-      this.loanType = var4;
-      this.applicationDate = var5;
-      this.loanStatus = "Pending";
-      this.verified = false;
-   }
+    private String loanId;
+    private Customer customer;
+    // The total loan amount that the loan has been alloted for
+    private double loanAmount;
+    private double interestRate;
+    private int loanTerm;
+    // The remaining amount that the customer has to pay to finish the loan
+    private double balance;
+    // The installment that is required to be paid every month for the loan
+    // If this installment is not paid regularly, then lateFee is acquired
+    private double installment;
+    private LocalDate loanStart;
+    private String status;
+    private double lateFee;
 
-   private String generateApplicationId() {
-      String var1 = String.format("%09d", applicationCounter);
-      ++applicationCounter;
-      return "LAP" + var1;
-   }
+    public LoanAccount(String loanId, Customer customer, double loanAmount, double interestRate, int loanTerm, LocalDate loanStart) {
+        this.loanId = loanId;
+        this.customer = customer;
+        this.loanAmount = loanAmount;
+        this.interestRate = interestRate;
+        this.loanTerm = loanTerm;
+        this.loanStart = loanStart;
+        this.balance = loanAmount;
+        this.status = "Active";
+        this.lateFee = 500;
+        this.installment = calculateInstallment();
+    }
 
-   public String getLoanStatus() {
-      return this.loanStatus;
-   }
+    public void getLoanDetails() {
+        System.out.println("Loan ID: " + loanId);
+        System.out.println("Customer: " + customer);
+        System.out.println("Loan Amount: " + loanAmount);
+        System.out.println("Interest Rate: " + interestRate);
+        System.out.println("Loan Term (months): " + loanTerm);
+        System.out.println("Balance: " + balance);
+        System.out.println("Installment: " + installment);
+        System.out.println("Loan Start Date: " + loanStart);
+        System.out.println("Status: " + status);
+        System.out.println("Late Fee: " + lateFee);
+    }
 
-   public void getLoanDetails() {
-      System.out.println("Loan Application ID: " + this.applicationId);
-      System.out.println("Customer: " + this.customer.getName());
-      System.out.println("Loan Amount: " + this.loanAmount);
-      System.out.println("Loan Type: " + this.loanType);
-      System.out.println("Application Date: " + String.valueOf(this.applicationDate));
-      PrintStream var10000 = System.out;
-      String var10001 = String.valueOf(this.approvalDate != null ? this.approvalDate : "Not Approved");
-      var10000.println("Approval Date: " + var10001);
-      var10001 = this.loanStatus;
-      System.out.println("Status: " + var10001);
-      System.out.println("Verified: " + (this.verified ? "Yes" : "No"));
-      var10001 = this.verifiedBy != null ? this.verifiedBy.getEmployeeName() : "Not Verified";
-      System.out.println("Verified By: " + var10001);
-   }
+    public String getLoanId() {
+        return this.loanId;
+    }
 
-   public String getCustomerName() {
-      return this.customer.getName();
-   }
+    public Customer getCustomer() {
+        return customer;
+    }
 
-   public void updateLoanStatus(String var1) {
-      this.loanStatus = var1;
-      if ("Approved".equalsIgnoreCase(var1)) {
-         this.approvalDate = LocalDate.now();
-      } else {
-         this.approvalDate = null;
-      }
+    public double getLoanAmount() {
+        return loanAmount;
+    }
 
-   }
+    public double getInterestRate() {
+        return interestRate;
+    }
 
-   public void updateVerification(boolean var1, Employee var2) {
-      this.verified = var1;
-      this.verifiedBy = var2;
-   }
+    public int getLoanTerm() {
+        return loanTerm;
+    }
 
-   public boolean isVerified() {
-      return this.verified;
-   }
+    public double getBalance() {
+        return balance;
+    }
 
-   public String getApplicationId() {
-      return this.applicationId;
-   }
+    public double getInstallment() {
+        return installment;
+    }
+
+    public LocalDate getLoanStart() {
+        return loanStart;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public double getLateFee() {
+        return lateFee;
+    }
+
+    public LocalDate getLoanEnd() {
+        return loanStart.plusMonths(loanTerm);
+    }
+
+    private double calculateInstallment() {
+        double monthlyInterestRate = interestRate / 12 / 100;
+        double installmentAmount = this.balance * monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -loanTerm));
+        return installmentAmount;
+    }
+
+    public void closeLoan() {
+        if (balance == 0) {
+            status = "Closed";
+            System.out.println("Loan has been closed");
+        } else {
+            System.out.println("Remaining amount " + balance + " still needs to be paid");
+        }
+    }
+
+    public void updateLateFee(double newLateFee) {
+        this.lateFee = newLateFee;
+        System.out.println("Late fee updated to: " + lateFee);
+    }
+
+    public void updateInstallment() {
+        this.installment = calculateInstallment();
+        System.out.println("Installment recalculated: " + installment);
+    }
+
+    public void updateTerm(int newLoanTerm) {
+        this.loanTerm = newLoanTerm;
+        this.installment = calculateInstallment();
+        System.out.println("Loan term updated to " + loanTerm + " months. New installment: " + installment);
+    }
+
+    public void updateInterest(double newInterestRate) {
+        this.interestRate = newInterestRate;
+        this.installment = calculateInstallment();
+        System.out.println("Interest rate updated to " + interestRate + "%. New installment: " + installment);
+    }
+
+    public void makePayment(Double amount) {
+        if (amount == null || amount < 0) {
+            System.out.println("Enter a valid amount");
+        } 
+        else if (amount == 0) {
+            System.out.println("Payment amount cannot be zero");
+        }
+        else if (amount > balance) {
+            System.out.println("Invalid amount. You are paying more than required amount");
+        }
+        else {
+            balance = balance - amount;
+            System.out.println("Payment of " + amount + " made. Remaining balance to be paid: " + balance);
+        }
+
+        if(balance == 0) {
+            System.out.println("Entire loanAmount has been paid");
+            closeLoan();
+        }
+    }
+
+    public void applyDelay() {
+        balance += lateFee;
+        System.out.println("Delay fee of " + lateFee + " applied. New amount to be paid: " + balance);
+    }
+
+    public void increaseLoan(double additionalAmount) {
+        if ("Active".equals(status)) {
+            this.loanAmount += additionalAmount;
+            this.balance += additionalAmount;
+            this.installment = calculateInstallment();
+            System.out.println("Loan increased by " + additionalAmount + ". New balance: " + balance);
+        } else {
+            System.out.println("Cannot increase loan on closed or inactive accounts");
+        }
+    }
 }
